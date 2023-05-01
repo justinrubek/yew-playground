e70fcd8d751f9ae0568a5a20a1e3b95811a1684f
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-05-01 00:09:03 -0500

    refactor: use functional component

--- crates/app/src/app.rs
+++ crates/app/src/app.rs
@@ -1,26 +1,10 @@
-use yew::prelude::*;
+use yew::{function_component, html, Html};
 
-pub struct App {}
-
-pub enum Msg {}
-
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        App {}
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        true
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <>
-                <h1>{ "Hello World!" }</h1>
-            </>
-        }
+#[function_component]
+pub fn App() -> Html {
+    html! {
+        <div>
+            <h1>{ "Hello World!" }</h1>
+        </div>
     }
 }
